249e9cd87b56d2c0419a7c2db1534c23c2cfb0be
rampia perente <157977666+perente@users.noreply.github.com>
2024-11-17 12:03:02 +0300

    Update server.py

--- server/server.py
+++ server/server.py
@@ -67,7 +67,55 @@ def get_disciplines():
             cursor.close()
             connection.close()
 
+@app.route('/events', methods=['GET'])
+def get_events():
+    try:
+        # Establish database connection
+        connection = db_connection()
 
+        if connection.is_connected():
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM Events")
+            events = cursor.fetchall()
+
+            # Return data as JSON
+            return jsonify(events), 200
+        else:
+            return jsonify({'error': 'Failed to connect to the database'}), 500
+
+    except Error as e:
+        return jsonify({'error': str(e)}), 500
+
+    finally:
+        # Close the connection
+        if 'connection' in locals() and connection.is_connected():
+            cursor.close()
+            connection.close()
+
+@app.route('/countries', methods=['GET'])
+def get_countries():
+    try:
+        # Establish database connection
+        connection = db_connection()
+
+        if connection.is_connected():
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM Country")
+            countries = cursor.fetchall()
+
+            # Return data as JSON
+            return jsonify(countries), 200
+        else:
+            return jsonify({'error': 'Failed to connect to the database'}), 500
+
+    except Error as e:
+        return jsonify({'error': str(e)}), 500
+
+    finally:
+        # Close the connection
+        if 'connection' in locals() and connection.is_connected():
+            cursor.close()
+            connection.close()
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
