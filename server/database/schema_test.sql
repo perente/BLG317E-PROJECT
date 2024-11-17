ad4e9b38cf8dee56d1f967e1cfcb6722ae625cc8
rampia perente <157977666+perente@users.noreply.github.com>
2024-11-17 11:54:20 +0300

    Update schema_test.sql

--- server/database/schema_test.sql
+++ server/database/schema_test.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS `Schedules`;
 DROP TABLE IF EXISTS `Discipline`;
+DROP TABLE IF EXISTS `Country`;
+DROP TABLE IF EXISTS `Events`;
 
 
 CREATE TABLE `Schedule` (
@@ -24,3 +26,23 @@ CREATE TABLE `Discipline` (
     PRIMARY KEY (`id`),
     UNIQUE KEY `unique_discipline_code` (`discipline_code`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE `Country` (
+    `country_code` CHAR(3) NOT NULL,
+    `country_name` VARCHAR(255) NOT NULL,
+    `country_long` VARCHAR(255),
+    `gold_medal` INT DEFAULT 0,
+    `silver_medal` INT DEFAULT 0,
+    `bronze_medal` INT DEFAULT 0,
+    `total` INT GENERATED ALWAYS AS (`gold_medal` + `silver_medal` + `bronze_medal`) STORED,
+    PRIMARY KEY (`country_code`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE `Events` (
+    `events_code` INT NOT NULL AUTO_INCREMENT,
+    `event_name` VARCHAR(255) NOT NULL,
+    `url` VARCHAR(255),
+    `discipline_code` CHAR(3),
+    `sport_name` VARCHAR(255),
+    PRIMARY KEY (`events_code`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci AUTO_INCREMENT = 0;
