c44980850742633a164f1a8ab2303bbe1070324e
rampia perente <157977666+perente@users.noreply.github.com>
2024-12-01 23:18:40 +0300

    Update routes.js

--- frontend/src/lib/routes.js
+++ frontend/src/lib/routes.js
@@ -3,10 +3,12 @@ const BASE_URL = 'http://localhost:8080/';
 
 export const APP_ROUTES = {
     schedules: '/schedules',
+    events: '/events',
 }
 
 export const API_ROUTE = {
     schedules: BASE_URL + 'schedules',
     events: BASE_URL + 'events',
     disciplines: BASE_URL + 'disciplines',
+    countries: BASE_URL + 'countries',
 }
